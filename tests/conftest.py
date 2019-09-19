eca0254c78919e2c0773a532d35d60d5723090a0
Shashi Kumar Raja <shashiraja92@gmail.com>
2019-09-19 22:46:06 +0530

    added allure-report service in docker to serve test report

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,6 +10,9 @@ from src import create_app
 
 @pytest.fixture(scope='module')
 def test_client():
+    """
+    Yields a test_client to be used with each test
+    """
     flask_app = create_app('testing')
 
     # Flask provides a way to test your application by exposing the Werkzeug test Client
@@ -26,6 +29,9 @@ def test_client():
 
 @pytest.fixture(scope='module')
 def load_json_schema():
+    """
+    Reads the stored json schema file available at a given path
+    """
     def _load_json_schema(filename):
         """ Loads the given schema file """
         relative_path = join('schemas', filename)
